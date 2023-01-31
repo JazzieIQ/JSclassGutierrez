62030dd9cfb31456267cf23afb78ec3eddaac35b
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-01-30 20:32:59 -0800

    update

---
+++ 003/module1.js
@@ -0,0 +1,4 @@
+alert("Wait. Just Wait, ok thanks.");
+const myAlert = "Hello World! K bye, he he he.";
+const myConsole = "hello world2222222";
+export {myAlert, myConsole};
--- 003/script.js
+++ 003/script.js
@@ -1,2 +1,1 @@
-alert("here is my alert yes yes yes");
-console.log("here's the console loggggggg");
+alert("Wait. Just Wait, ok thanks.");
