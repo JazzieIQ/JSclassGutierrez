1e2116b0235ca303d91506cf377ea8e9f0fdefb8
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-02-08 17:27:05 -0800

    update

---
+++ 004/beginner-html-site-styled-gh-pages/main2.js
@@ -0,0 +1,5 @@
+let foo = 42
+console.log("Foo is bar which is tyoeof", (typeof foo))
+foo = "bar" console.log("Foo is true which is typeof", (typeof foo))
+foo = true console.log("Foo is true which is typeof", (typeof foo))
+Foo = 42 const result = Foo + "1" console.log ("result is 42 +1...", result)
