124eb3ee53924f7560da3fd0ab816cd2a69d8b2d
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-02-16 21:53:03 -0800

    more on variables

---
+++ 006/main.js
@@ -0,0 +1,16 @@
+let myName = "Mac Mac"
+let myAge = 32
+let myHappiness = true
+let newVar = 'something'
+const myConst = 42
+console.log (myAge == myName)
+console.log (myAge === myName)
+
+let myVar = null
+console.log('myVar is typeof ${typeof myVar}')
+
+console.log('myName is typeof ${typeof myName}')
+console.log('myName is value ${myName}')
+console.log('myAge is typeof ${typeof myAge}')
+
+// if (true) {const myConst = 43 console.log (myConst)} else {} //
