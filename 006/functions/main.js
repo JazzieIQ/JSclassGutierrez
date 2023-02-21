f259d2550eda76391c50ce631a8c11e9572dca6d
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-02-21 15:02:47 -0800

    functions are fun

--- 006/functions/main.js
+++ 006/functions/main.js
@@ -1,25 +1,8 @@
 // Functions are fun!
 
-function doMath (a) {
-    let result = a
-    console.log ('this is the result ${a}')
+function doMath(a, b) {
+    let result = a * b
+    console.log ('this is the result ' + result)
 }
 
-doMath (10)
-let myName = "Mac Mac"
-let myAge = prompt ("What is one's age?")
-let myHappiness = true
-let newVar = 'something'
-const myConst = 42
-let myVar = null
-console.log (myAge == myConst)
-console.log (myAge === myName)
-console.log(typeof myVar)
-console.log(typeof myName)
-console.log(typeof myName)
-console.log(typeof myAge)
-
-if (true) { let myHappiness = false; console.log(myHappiness);}
-(false); {
-    console.log(myHappiness);
-}
+doMath(10, 4)
