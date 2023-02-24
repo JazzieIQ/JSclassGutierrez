b4839d005e8a694ccbf99f0f147de9c8643b9516
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-02-23 19:54:50 -0800

    variables

--- 006/main.js
+++ 006/main.js
@@ -1,17 +1,34 @@
-let myName = "Mac Mac"
-let myAge = prompt ("What is one's age?")
-let myHappiness = true
-let newVar = 'something'
+
 const myConst = 42
-let myVar = null
-console.log (myAge == myConst)
-console.log (myAge === myName)
-console.log(typeof myVar)
-console.log(typeof myName)
+const happiness = true
+let myHappiness = happiness
+function myName() {
+    var stringName = prompt("Give your name please?")
+    return stringName;
+}
+$scope.userInput = function (stringName) {
+    var regPattern = /^[A-Za-z]{2,}$/;
+    var regResult = regPattern.test(stringName);
+    if (regResult === false) {
+        alert("I'm not quite sure that is your name.")
+        return stringName
+    }
+    else {
+        alert(`Thank You ${myName}`)
+        return stringName;}
+
+}
+function myAge() {
+    var stringName = prompt("Give your name please?")
+    return stringName;
+}
+let stringName = myName 
 console.log(typeof myName)
 console.log(typeof myAge)
-
-if (true) { let myHappiness = false; console.log(myHappiness);}
-(false); {
-    console.log(myHappiness);
+console.log(stringName === String)
+if (true) {
+    let myAge = function prompt(String { "What is one's age?"}) 
 }
+    let log1 = String(myAge + 'and' + myNAme + 'are not the same.')
+    console.log(log1 + myAge === myName)
+
