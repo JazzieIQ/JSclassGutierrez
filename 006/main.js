296e487b2519f811a761d3be9ee1598d2af0ac12
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-02-23 22:13:28 -0800

    return of functions

--- 006/main.js
+++ 006/main.js
@@ -1,4 +1,3 @@
-
 const myConst = 42
 const happiness = true
 let myHappiness = happiness
@@ -6,29 +5,37 @@ function myName() {
     var stringName = prompt("Give your name please?")
     return stringName;
 }
-$scope.userInput = function (stringName) {
+function stringName (myName) {
     var regPattern = /^[A-Za-z]{2,}$/;
-    var regResult = regPattern.test(stringName);
+    var regResult = regPattern.test(myName);
     if (regResult === false) {
         alert("I'm not quite sure that is your name.")
-        return stringName
+        return stringName;
     }
     else {
         alert(`Thank You ${myName}`)
-        return stringName;}
-
-}
-function myAge() {
-    var stringName = prompt("Give your name please?")
-    return stringName;
+        return myName;}
 }
 let stringName = myName 
 console.log(typeof myName)
-console.log(typeof myAge)
-console.log(stringName === String)
-if (true) {
-    let myAge = function prompt(String { "What is one's age?"}) 
+function myAge() {
+    var intAge = prompt("What is one's age?")
+    return intAge;
 }
-    let log1 = String(myAge + 'and' + myNAme + 'are not the same.')
-    console.log(log1 + myAge === myName)
-
+myAge.userInput = function (intAge)
+    {
+    var regPattern = /^[0-9]{2,}$/;
+    var intResult = regPattern.test(intAge);
+    if (intResult === false) {
+        alert("I'm sorry I need a number.")
+        return intAge
+    }
+    else {
+        let log1 = String(myAge + 'and' + myHappiness + 'are not the same.')
+        console.log(log1)
+        console.log(typeof myAge)
+        console.log(myAge === myHappiness)
+        return intAge;
+    }    
+}
+console.log(stringName === String)
