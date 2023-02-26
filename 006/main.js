a0fef54c6e156f5382fdb30c43c53bd2b74a6a18
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-02-25 17:42:43 -0800

    functions

--- 006/main.js
+++ 006/main.js
@@ -1,32 +1,24 @@
 const myConst = 42
 const happiness = true
 let myHappiness = happiness
-let myName = prompt("Give your name please?", "Absolum"); 
+let myName = prompt('Give your name please?', 'Absolum') 
 let myAge = prompt("What is one's age?", "00");
 function stringName() {
-                var stringName = myName
-                var namePattern = /^[A-Z][a-z]{0,50}$/;
-                var nameResult = namePattern.test(stringName);
-                if (nameResult === true) {
+                var stringName = /^[A-Za-z]+$/;
+                if (myName.valueOf===stringName) {
                     alert(`Thank You ${stringName}`);
-
                 }
                 else {
                     alert("I'm not quite sure that is your name.");
     }
+    console.log(stringName)
+    return stringName
 }
-        try{
-            int number = Integer.parseInt(myAge);
-            System.out.println(number); // output = 25
-        }
-        catch (NumberFormatException ex){
-            ex.printStackTrace();
-        }
 function intAge(myAge)
-    {
-    var intAge = myAge
-    var regPattern = /^[0-9]{0,2}$/;
-    var intResult = regPattern.test(intAge);
+{
+    var intAge = parseInt(myAge); // output = 25
+    var intAge = /^[0-9]+$/;
+    var intResult = intAge.test(intAge);
     if (intResult === false) {
         alert("I'm sorry I need a number.");
         console.log("Needs a number")
@@ -38,11 +30,13 @@ function intAge(myAge)
         [
             alert(`${myAge}and${myHappiness}are not the same.`)
         ]
+    System.out.println(myAge);
     return intAge
 }
+
 console.log(typeof myName)
 console.log(typeof myAge)
 console.log(typeof happiness)
 console.log(typeof myConst)
 console.log(stringName === myName)
-console.log(myAge === myHappiness)
+console.log(myAge == myHappiness);
