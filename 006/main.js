62b1f80253c046a90b16c72f1e97eb71ea1b0854
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-02-26 17:25:42 -0800

    functions

--- 006/main.js
+++ 006/main.js
@@ -2,40 +2,48 @@ const myConst = 42
 const happiness = true
 let myHappiness = happiness
 let myName = prompt('Give your name please?', 'Absolum') 
-let myAge = prompt("What is one's age?", "00");
-function stringName() {
-                var stringName = /^[A-Za-z]+$/;
-                if (myName.valueOf===stringName) {
-                    alert(`Thank You ${stringName}`);
-                }
-                else {
-                    alert("I'm not quite sure that is your name.");
-    }
-    console.log(stringName)
-    return stringName
+function testName(myName)
+    {
+        if (myName.match(/^[A-Za-z]+$/)) 
+        {
+            alert(`Thank You ${myName}`)
+        }
+        else {
+            alert("I'm not quite sure that is your name.");
+            myName
+        }
+
 }
+console.log("Their name is " + myName)
+testName(myName)
+let myAge = prompt("What is one's age?", "00");
+console.log("Their age is" + myAge)
 function testAge(myAge)
 {
-    var intAge = String.fromCharCode(myAge.which)
+    //let intAge = 
+    // String.fromCharCode(myAge)
    // var intResult = intAge.test(intAge);
-    if (!(/^[0-9]+$/.test(myAge))) {
+    if (myAge.match(/^[0-9]+$/)) {
         alert("I'm sorry I need a number.");
         console.log("Still need a number")
+        return myAge
     }
     else if (intAge === myConst ) {
         alert("Pure Bliss");
     }
     else
         [
-            alert(`${myAge}and${myHappiness}are not the same.`)
+            alert(`${myAge}and${myHappiness}are not the same.`),
+            myHappiness = false
         ]
-    System.out.println(myAge);
-    return intAge
+    System.out.println(myAge)
+    return intAge;
 }
 
+testAge(myAge)
 console.log(typeof myName)
 console.log(typeof myAge)
 console.log(typeof happiness)
 console.log(typeof myConst)
-console.log(stringName === myName)
+console.log(testName=== myName)
 console.log(myAge == myHappiness);
