180faf396bc44757f129c479fa16a484e62a906d
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-02-26 21:38:51 -0800

    functions

--- 006/main.js
+++ 006/main.js
@@ -1,38 +1,38 @@
 const myConst = 42
 const happiness = true
 let myHappiness = happiness
-let myName = prompt('Give your name please?', 'Absolum') 
+let myName = prompt('Give your name please?', 'Absolum'); 
 function stringName(myName)
     {
-        if (myName.match(/^[A-Za-z]+$/)) 
-        {
-            alert(`Thank You ${myName}`)
-        }
-        else {
+    if (myName.match(/^[A-Za-z]+$/)) {
+        alert(`Thank You ${myName}`)
+    }
+    else {
             alert("I'm not quite sure that is your name.");
-            myName
+        myName = prompt('Give your name please?', 'Absolum');
+        stringName(myName)
         }
 
 }
-console.log("Their name is " + myName)
 stringName(myName)
+console.log("Their name is " + myName)
 let myAge = prompt("What is one's age?", "00");
 function intAge(myAge)
 {
     parseInt(myAge)
     return myAge;
 }
-console.log("Their age is " + myAge)
-
 intAge(myAge)
-
-function testAge(myAge)
+console.log("Their age is " + myAge)
+function testAge(intAge)
 {
     if (!(myAge.match(/^[0-9]+$/)))
     {
         alert("I'm sorry I need a number.");
         console.log("Still need a number")
-        return myAge
+        myage = prompt("What is one's age?", "00");
+        intAge(myAge)
+        testAge(intAge)
     }
     else if (myAge === myConst ) {
         alert("Pure Bliss");
