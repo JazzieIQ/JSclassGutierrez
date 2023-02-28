02eb394a13f6707d5fdc3365d11b2be6e673bf4e
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-02-27 16:55:14 -0800

    functions

--- 006/main.js
+++ 006/main.js
@@ -1,19 +1,20 @@
 const myConst = 42
 const happiness = true
 let myName = prompt('Give your name please?', 'Absolum'); 
-function stringName(myName)
+function testName(myName)
     {
-    if (myName.match(/^[A-Za-z]+$/)) {
-        alert(`Thank You ${myName}`)
+    if (!(typeof myName == String))
+    {
+        alert("Thank You " + [myName])
     }
     else {
             alert("I'm not quite sure that is your name.");
-        myName = prompt('Give your name please?', 'Absolum');
-        stringName(myName)
+            myName = prompt('Give your name please?', 'Absolum');
+            stringName(myName)
         }
-
 }
-stringName(myName)
+let stringName = testName(myName)
+testName(myName)
 console.log("Their name is " + myName)
 let myAge = prompt("What is one's age?", "00");
 function determineIntAge(myAge)
@@ -21,49 +22,70 @@ function determineIntAge(myAge)
         return parseInt(myAge)
 
 }
-determineAge(myAge)
-console.log("Their age is " + intAge(myAge))
+let intAge = determineIntAge(myAge)
+console.log("Their age is " + [intAge])
 function testAge(intAge)
 {
-    if (!(myAge.match(/^[0-9]+$/)))
+    if (!(typeof intAge == typeof myConst))
+        //!(intAge.match(/^[0-9]+$/,)))
+        //!(intAge == myConst))
+        //RegExp.prototype.test(newMyage) (!(/^[0-9]+$/,)))
+        
+    {
+        alert("I'm sorry I need a number.")
+        console.log("Still need a number")
+        myage = prompt("What is one's age?", "00")
+        determineIntAge(myAge)
+        testAge(intAge)
+    }
+    else if (intAge = null)
     {
         alert("I'm sorry I need a number.");
         console.log("Still need a number")
         myage = prompt("What is one's age?", "00");
-        intAge(myAge)
+        determineIntAge(myAge)
         testAge(intAge)
     }
-    else if (myAge === myConst ) {
-        alert("Pure Bliss");
+    else if (intAge === myConst) {
+        alert("Pure Bliss. I feel it is very " + happiness + " that you are happy");
     }
+
     else
         [
             alert("I feel it is " + happiness + " that you are happy"),
         ]
-
 return intAge;
 }
-let myHappiness = function testHappiness(myHappiness) {
+testAge(intAge)
+//happiness is a constant that we will use to measure how joyous the user feels through their age input.
+//Clever users will know "42" is the best Answer regardless of their true age.
+//Clever programmers will log what they really know.
+let myHappiness = happiness
+function testHappiness(myHappiness) {
     if (intAge == myConst) (
+        // myHappiness = happiness remains
         myHappiness = true
     )
     else if (intAge === myConst) {
+        // myHappiness = happiness remains
         myHappiness = true
         console.log("ROFL! They made a funny.")
     }
     else if (!(intAge == myConst)) {
-        myHappiness = false
+        // now myHappiness must be false
+        let myHappiness = false
         console.log("I don't think I know if they are happy.")
         return myHappiness;
     }
     else (!(intAge === myConst))
     {
-        myHappiness = false
+        // now myHappiness must be false
+        let myHappiness = false
         console.log("I don't think they are happy.")
         return myHappiness;
     }
 }
-myHappiness(happiness)
+testHappiness(myHappiness)
 console.log(happiness)
 console.log(myHappiness === happiness)
 console.log(typeof myName)
