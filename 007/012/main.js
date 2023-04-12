e88ad3a8a94daf3bedeaba56c7263e3602705c4d
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-04-11 23:01:46 -0700

    updating the DOM with my changes.

--- 007/012/main.js
+++ 007/012/main.js
@@ -1,12 +1,17 @@
 //update the DOM
 
 const updateDOM = (input) => {
-    const divEl = document.querySelector('#output')
-    const p = document.createElement('p')
-    p.textContent = input
-    divEl.appendChild(p)
+    if (kingsInquiry == "kingly answer") {
+        var divEl = document.querySelector('#output')
+        var p = document.createElement('p')
+        p.textContent = input
+        divEl.appendChild(p);
+    }
+    else {
+        alert("THOU HAS FAILED THE KING!")
+        kingsInquiry();
+    }
 }
-
-const mileage = (miles, gallons, price = 3.79) => miles/gallons * price 
-
-updateDOM()
+const kingsInquiry = prompt("Respond for the King...", "kingly answer")
+const kinglyAnswer = "African or European?"
+updateDOM(kinglyAnswer)
