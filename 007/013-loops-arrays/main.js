c465ebdc837db753130d4b9d70d81c8af544f050
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-04-21 14:00:12 -0700

    my code for averag trip cost

--- 007/013-loops-arrays/main.js
+++ 007/013-loops-arrays/main.js
@@ -1,43 +1,234 @@
-const MY_MPG = []
-const MY_TRIP_COST = []
+// A Bridge Too Far: The Kings Inquiry HTML/Text Based Game.
 
-const updateDOM = (input) => {
-    const divEl = document.querySelector('#output')
-    const p = document.createElement('p')
-    p.textContent = input
-    divEl.appendChild(p)
-}
+// Key phrasing
 
-const trackMPGandCost = (miles, gallons, price = 3.79) => {
-    const MPG  = Math.round(miles/gallons)
-    const tripCost = Math.round(gallons * price)
-    updateDOM(`Miles per gallon  is ${MPG} and trip cost is ${tripCost}`)
-    MY_MPG.push(MPG)
-    MY_TRIP_COST.push(tripCost)
-}
+const kinglyAnswer = '"African or European?"';
+var count = 0;
+var rateCount = 0;
 
-const calculateSUM = (arr) => {
-    let sum = 0
-    for(let i=0; i < arr.length; i++) {
-        sum = sum + arr[i]
+//console.log (document.getElementById('output'))
+const updateDOM = (kingsInquiry) => {
+    if (kingsInquiry == "African or European?") {
+        var divEl = document.querySelector('#output')
+        var p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;    
+        //console.log(rateCount);
+        return;
     }
-    return sum
+    else if (kingsInquiry == "I am King I'm supposed to know these things.") {
+        var divEl = document.querySelector('#output')
+        var p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;    
+        //console.log(rateCount);
+        return;
+    }
+    else if (kingsInquiry == "a kingly answer") {
+        var divEl = document.querySelector('#output')
+        var p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        return;
+    }
+    else if (kingsInquiry == "five") {
+        var divEl = document.querySelector('#output')
+        var p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        return;
+    }
+    else if (kingsInquiry == "Holy Hand Grenade of Antioch") {
+        var divEl = document.querySelector('#output')
+        var p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;;
+        return;
+    }
+    else if (kingsInquiry == "I am Authur, King of the Britains.") {
+        var divEl = document.querySelector('#output')
+        var p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        return;
+    }
+    else if (kingsInquiry == "I am your King") {
+        var divEl = document.querySelector('#output')
+        var p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        return;
+    }
+    else {
+        var divEl = document.querySelector('#output')
+        var p = document.createElement('p')
+        p.textContent = "The hour is past and thou art DOOMED!!!!!"
+        divEl.appendChild(p);
+        alert("THOU HAST FAILED THE KING!");
+        rateCount;
+        return;
+    }
 }
 
-const calculateAvg = () => {
-    let sumMPG = calculateSUM(MY_MPG)
-    let sumTripCost = calculateSUM(MY_TRIP_COST)
-    let avgMPG = Math.round(sumMPG/MY_MPG.length)
-    let avgTripCost = Math.round(sumTripCost/MY_TRIP_COST.length)
-    updateDOM(`Average MPG is ${avgMPG}`)
-    updateDOM(`Average Trip is ${avgTripCost}`)
-}
+var buttonCount = document.getElementById("replay"),
+  count;
+replay.onclick = function() {
+  count += 1;
+    replay.innerHTML = "Attempt: " + count;
+    return count;
+};
+
+//update the DOM
+const updateDOMArray = (kingsInquiry) => {
+        var divEl = document.querySelector('#array')
+        var a = document.createElement('a')
+    a.textContent = '"' + "The King's answer was: " + "'" + kingsInquiry + "' " + '"; '
+    divEl.appendChild(a);
+        return;
+        }
+
+// Play the game/ attemp again
+
+document.querySelector('#replay').addEventListener('click', () => {
+    buttonCount;
+    var kingsInquiry = window.prompt("Consider wisley as thou speaks for the King...", "I am King I'm supposed to know these things.");
+    var scribe = console.log("They transcribed " + '"' + kingsInquiry + '"');
+    scribe;
+    kinglyAnswer;
+    updateDOM(kingsInquiry);
+
+    //div "output" split into array. Array increase with attempt.
+
+    updateDOMArray(kingsInquiry);
+    const kingsRecord = (document.getElementById("array")).textContent.split('; ');
+    const noNullKingsArray = kingsRecord.filter(element => element !== '');
+    console.log(noNullKingsArray);
+    noNullKingsArray.push('I told you that you were doomed! Oh but nobody listens to me!');
+    
+    // calculate success rate
+    
+    const rateSuccess = rateCount / count;
+    let appendDivElRate = "; Success Rates Array: ";
+    const rateSuccessArray = [];
+    const success = rate.innerHTML = "Success: " + rateSuccess + appendDivElRate;
+    success;
+    document.getElementById("rate").append(rateSuccessArray);
+    console.log(rateSuccessArray);
+
+    // Give success rate Array with For Loop.
+    const win = 1;
+    const lose = 0;
+    let i = rateSuccessArray.length;
+    for (i = 0; i <= count; i++) {
+        //console.log(updateDOM);
+            if (rateCount == count) {
+            //win;
+            rateSuccessArray.push(win);
+        }
+        else if (rateCount < count) {
+            //lose;
+            rateSuccessArray.push(lose);
+        }
+        else {
+            null;
+        }    
+    };
+    document.getElementById('rate').append(rateSuccessArray);
+    return;
+});
+
+//refresh Output div. All code is reset including the array.
+
+document.querySelector('#refresh').addEventListener('click', () => {
+    document.getElementById("output").innerHTML = "";
+})
+
+// unused code
+/*
 
 
-trackMPGandCost(360, 15, 5.40)
-trackMPGandCost(320, 12, 5)
-trackMPGandCost(100, 7, 4.40)
-trackMPGandCost(600, 24, 5.70)
-trackMPGandCost(50, 2, 3.40)
-trackMPGandCost(320, 12, 5)
-calculateAvg()
+        if (kingsInquiry.includes(kinglyAnswer)) {
+            array.forEach(kingsInquiry.includes(kinglyAnswer) => {
+                successResult += 1;
+                const elRate = document.getElementById('rate');
+                const text = document.createTextNode(rateSuccessArray.push(successArray));
+                console.log(rateSuccessArray);
+                elRate.appendChild(text);
+            });
+        }
+        else(!(kingsInquiry.includes(kinglyAnswer))) {
+            array.forEach(kingsInquiry.includes("ratecount;") => {
+            successResult += 0;
+            const elRate = document.getElementById('rate');
+            const text = document.createTextNode(rateSuccessArray.push(successArray));
+            console.log(rateSuccessArray);
+            elRate.appendChild(text);
+            });
+        }
+const successRateUpdate = document.getElementById("refresh").onclick = function () {
+
+    return;
+};
+
+//    const updateArray = kingsArray.replace("", 'I told you so, oh but nobody listens to me!' )
+
+    // updateDOMArray(kingsInquiry);
+    const updateDOMArray = [kingsInquiry];
+    updateDOMArray.push("I told you so, oh but nobody listens to me!");
+
+const updateDOMArray = (kingsInquiry) => {
+        var divEl = document.querySelector('#array')
+        var a = document.createElement('a')
+    a.textContent = '"' + "The King's answer was: " + "'" + kingsInquiry + "' " + '"; '
+        divEl.appendChild(a);
+        return;
+        }
+
+//    const kingsArray = [];
+//    kingsArray.push(kingsRecord);
+//    console.log(kingsArray);
+    
+var outputJs = document.getElementById("output");
+    var kingsRecord = console.log(outputJs.textContent.split('/n'));
+    kingsRecord;
+    const kingsArray = []
+    kingsArray.push(kingsRecord);
+    console.log(kingsArray);
+
+var kingsInquiry = window.prompt("Consider wisley as thou speaks for the King...", "a kingly answer");
+const domResult = updateDOM(kingsInquiry.textContent);
+console.log(valueOf(domResult));
+
+var outputJs = document.getElementById("output");
+var kingsRecord = console.log(outputJs.textContent.split('null'));
+//kingsRecord;
+var kingsArray = console.log(kingsRecord.textContent.split('\n'));
+
+kingsArray;
+    var outputJs = document.getElementById("output");
+    var kingsRecord = console.log(outputJs.textContent.split('null' + "<br/>"));
+    kingsRecord;
+
+    const kingsArray = []
+    kingsArray.push(kingsRecord);
+    console.log(kingsArray);
+
+
+    var kingsRecord = document.getElementById("output");
+    var kingsArray = console.log(kingsRecord.textContent.split('/n'));
+    kingsArray;
+     */
