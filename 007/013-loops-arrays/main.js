78265a92244a9940c39bca90e44e40377d6f74fa
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-04-24 23:20:17 -0700

    refactoring and readm commentsg

--- 007/013-loops-arrays/main.js
+++ 007/013-loops-arrays/main.js
@@ -3,39 +3,38 @@
 // Key phrasing
 
 const kinglyAnswer = '"African or European?"';
-var count = 0;
-var rateCount = 0;
+let count = 0;
+let rateCount = 0;
 const rateSuccessArray = [];
 const win = 1;
 const lose = 0;
     
-//console.log (document.getElementById('output'))
+//updateDOM
+
 const updateDOM = (kingsInquiry) => {
     if (kingsInquiry == "African or European?") {
-        var divEl = document.querySelector('#output')
-        var p = document.createElement('p')
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
         p.textContent = "The King's answer be: " + kinglyAnswer
         divEl.appendChild(p);
         rateCount;
         rateCount += 1;
         rateSuccessArray.push(win);
-        //console.log(rateCount);
         return;
     }
     else if (kingsInquiry == "I am King I'm supposed to know these things.") {
-        var divEl = document.querySelector('#output')
-        var p = document.createElement('p')
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
         p.textContent = "The King's answer be: " + kinglyAnswer
         divEl.appendChild(p);
         rateCount;
         rateCount += 1;
         rateSuccessArray.push(win);
-        //console.log(rateCount);
         return;
     }
     else if (kingsInquiry == "a kingly answer") {
-        var divEl = document.querySelector('#output')
-        var p = document.createElement('p')
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
         p.textContent = "The King's answer be: " + kinglyAnswer
         divEl.appendChild(p);
         rateCount;
@@ -44,8 +43,8 @@ const updateDOM = (kingsInquiry) => {
         return;
     }
     else if (kingsInquiry == "five") {
-        var divEl = document.querySelector('#output')
-        var p = document.createElement('p')
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
         p.textContent = "The King's answer be: " + kinglyAnswer
         divEl.appendChild(p);
         rateCount;
@@ -54,8 +53,8 @@ const updateDOM = (kingsInquiry) => {
         return;
     }
     else if (kingsInquiry == "Holy Hand Grenade of Antioch") {
-        var divEl = document.querySelector('#output')
-        var p = document.createElement('p')
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
         p.textContent = "The King's answer be: " + kinglyAnswer
         divEl.appendChild(p);
         rateCount;
@@ -64,8 +63,8 @@ const updateDOM = (kingsInquiry) => {
         return;
     }
     else if (kingsInquiry == "I am Authur, King of the Britains.") {
-        var divEl = document.querySelector('#output')
-        var p = document.createElement('p')
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
         p.textContent = "The King's answer be: " + kinglyAnswer
         divEl.appendChild(p);
         rateCount;
@@ -73,9 +72,9 @@ const updateDOM = (kingsInquiry) => {
         rateSuccessArray.push(win);
         return;
     }
-    else if (kingsInquiry == "I am your King") {
-        var divEl = document.querySelector('#output')
-        var p = document.createElement('p')
+    else if (kingsInquiry == "I am your King.") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
         p.textContent = "The King's answer be: " + kinglyAnswer
         divEl.appendChild(p);
         rateCount;
@@ -83,9 +82,49 @@ const updateDOM = (kingsInquiry) => {
         rateSuccessArray.push(win);
         return;
     }
+    else if (kingsInquiry == "You don't vote for kings.") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        return;
+    }
+    else if (kingsInquiry == "Be quiet!") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        return;
+    }
+    else if (kingsInquiry == "Now, stand aside worthy adversary!") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        return;
+    }
+    else if (kingsInquiry == "On second thought, let's not go to Camelot. 'Tis a silly place.") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        return;
+    }    
     else {
-        var divEl = document.querySelector('#output')
-        var p = document.createElement('p')
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
         p.textContent = "The hour is past and thou art DOOMED!!!!!"
         divEl.appendChild(p);
         alert("THOU HAST FAILED THE KING!");
@@ -95,20 +134,20 @@ const updateDOM = (kingsInquiry) => {
     }
 }
 
-var buttonCount = document.getElementById("replay"),
-  count;
-replay.onclick = function() {
-  count += 1;
+let buttonCount = document.getElementById("replay")
+replay.onclick = function () {
+    count;
+    count += 1;
     replay.innerHTML = "Attempt: " + count;
     return count;
 };
 
 //update the DOM
 const updateDOMArray = (kingsInquiry) => {
-        var divEl = document.querySelector('#array')
-        var a = document.createElement('a')
-    a.textContent = '"' + "The King's answer was: " + "'" + kingsInquiry + "' " + '"; '
-    divEl.appendChild(a);
+        let divEl = document.querySelector('#array')
+        let a = document.createElement('a')
+        a.textContent = '"' + "The King's answer was: " + "'" + kingsInquiry + "' " + '"; '
+        divEl.appendChild(a);
         return;
         }
 
@@ -116,8 +155,8 @@ const updateDOMArray = (kingsInquiry) => {
 
 document.querySelector('#replay').addEventListener('click', () => {
     buttonCount;
-    var kingsInquiry = window.prompt("Consider wisley as thou speaks for the King...", "I am King I'm supposed to know these things.");
-    var scribe = console.log("They transcribed " + '"' + kingsInquiry + '"');
+    let kingsInquiry = window.prompt("Consider wisley as thou speaks for the King...", "I am King I'm supposed to know these things.");
+    let scribe = console.log("They transcribed " + '"' + kingsInquiry + '"');
     scribe;
     kinglyAnswer;
     updateDOM(kingsInquiry);
@@ -132,20 +171,21 @@ document.querySelector('#replay').addEventListener('click', () => {
     
     // calculate success rate
     
-    const rateSuccess = (rateCount / count)*100;
+    const rateSuccess = (rateCount / count) * 100;
+    const ratePercent = Math.round(rateSuccess) + "%";
     
 
     // Give success rate Array with For Loop.
     let i = 0;
     let sumSuccesses = 0;
     let appendDivElRate = "; Success Rates Array: ";
-    let appendSuccessArray = "; This is the Your average via for Loop: ";
+    let appendSuccessArray = "; The scribe has thou averaged plainy: ";
         for (i = 0; i < rateSuccessArray.length; i++) {
         sumSuccesses = sumSuccesses + rateSuccessArray[i];
     };
     console.log(sumSuccesses)
     let rateCounting = sumSuccesses / count;
-    const success = rate.innerHTML = "Success: " + rateSuccess + "%" + appendDivElRate + rateSuccessArray + appendSuccessArray + rateCounting;
+    const success = rate.innerHTML = "Success: " + ratePercent + appendDivElRate + rateSuccessArray + appendSuccessArray + rateCounting;
     success;
 });
 
@@ -153,115 +193,4 @@ document.querySelector('#replay').addEventListener('click', () => {
 
 document.querySelector('#refresh').addEventListener('click', () => {
     document.getElementById("output").innerHTML = "";
-})
-
-// unused code
-/*
-
-    let i = rateSuccessArray.indexOf(count);
-    console.log(i)
-    let gettingOutputAgain = document.getElementById(output).textContent;
-    console.log(gettingOutputAgain);
-
-    gettingOutputAgain.forEach(kinglyAnswer => {
-        if (kinglyAnswer = true) {
-            rateSuccessArray.push(win);
-            console.log(rateSuccessArray);
-            document.getElementById('rate').append(rateSuccessArray);
-        }
-        else {
-            //lose;
-            rateSuccessArray.push(lose);
-            console.log(rateSuccessArray);
-            document.getElementById('rate').append(rateSuccessArray);
-        }
-
-    });
-//rateSuccessArray;
-if (rateCount == count) {
-            //win;
-            rateSuccessArray.push(win);
-            console.log(rateSuccessArray);
-            document.getElementById('rate').append(rateSuccessArray);
-            //return rateSuccessArray;
-        }
-        else (!(rateCount == count)) {
-            //lose;
-            rateSuccessArray.push(lose);
-            console.log(rateSuccessArray);
-            document.getElementById('rate').append(rateSuccessArray);
-            //return rateSuccessArray;
-        }
-        //console.log(updateDOM);
-
-        if (kingsInquiry.includes(kinglyAnswer)) {
-            array.forEach(kingsInquiry.includes(kinglyAnswer) => {
-                successResult += 1;
-                const elRate = document.getElementById('rate');
-                const text = document.createTextNode(rateSuccessArray.push(successArray));
-                console.log(rateSuccessArray);
-                elRate.appendChild(text);
-            });
-        }
-        else(!(kingsInquiry.includes(kinglyAnswer))) {
-            array.forEach(kingsInquiry.includes("ratecount;") => {
-            successResult += 0;
-            const elRate = document.getElementById('rate');
-            const text = document.createTextNode(rateSuccessArray.push(successArray));
-            console.log(rateSuccessArray);
-            elRate.appendChild(text);
-            });
-        }
-const successRateUpdate = document.getElementById("refresh").onclick = function () {
-
-    return;
-};
-
-//    const updateArray = kingsArray.replace("", 'I told you so, oh but nobody listens to me!' )
-
-    // updateDOMArray(kingsInquiry);
-    const updateDOMArray = [kingsInquiry];
-    updateDOMArray.push("I told you so, oh but nobody listens to me!");
-
-const updateDOMArray = (kingsInquiry) => {
-        var divEl = document.querySelector('#array')
-        var a = document.createElement('a')
-    a.textContent = '"' + "The King's answer was: " + "'" + kingsInquiry + "' " + '"; '
-        divEl.appendChild(a);
-        return;
-        }
-
-//    const kingsArray = [];
-//    kingsArray.push(kingsRecord);
-//    console.log(kingsArray);
-    
-var outputJs = document.getElementById("output");
-    var kingsRecord = console.log(outputJs.textContent.split('/n'));
-    kingsRecord;
-    const kingsArray = []
-    kingsArray.push(kingsRecord);
-    console.log(kingsArray);
-
-var kingsInquiry = window.prompt("Consider wisley as thou speaks for the King...", "a kingly answer");
-const domResult = updateDOM(kingsInquiry.textContent);
-console.log(valueOf(domResult));
-
-var outputJs = document.getElementById("output");
-var kingsRecord = console.log(outputJs.textContent.split('null'));
-//kingsRecord;
-var kingsArray = console.log(kingsRecord.textContent.split('\n'));
-
-kingsArray;
-    var outputJs = document.getElementById("output");
-    var kingsRecord = console.log(outputJs.textContent.split('null' + "<br/>"));
-    kingsRecord;
-
-    const kingsArray = []
-    kingsArray.push(kingsRecord);
-    console.log(kingsArray);
-
-
-    var kingsRecord = document.getElementById("output");
-    var kingsArray = console.log(kingsRecord.textContent.split('/n'));
-    kingsArray;
-     */
+});
