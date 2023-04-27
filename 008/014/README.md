20aedd788e346ec29212e8ca923c2f3d08009a4f
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-04-27 13:24:01 -0700

    form for mileageg

--- 008/014/README.md
+++ 008/014/README.md
@@ -1,24 +1,5 @@
-# Week 7
-# Are you using devTools?
-Why yes I use Google Chrome inspect features. I have used it to look at HTML and CSS elements and am currently picking up how to look at .js files in it now.
+# Week 8
 
-# "fixing issues"
-0
-I have a full working game. You can have multiple attemps and the div output will append with every attempt showing a funny quote or saying you are doomed. You can also reset the code to empty the div and start fresh.
+I have used forms and seen  validation issues. My code actually had similar issues as I needed to removed caps. So correct answers were not seen as valid because they were not typed in caps for caps as well required punctuation etc. I intend to add a filter and remoove caps from user input to make it more functional for my user.
 
-I started with your original code from video one with commmit update to dom.
-
-However in the first video we were instructed to change the code to fit our understanding of updating the DOM. So I did but it was obvious I went to a farther degree again. By the time I started the second video my code was too distinct from the mileage function.
-
-However I believe my code meets these assingments parameters. As i did not stray away from updating the DOM in div id= "output".
-Additonally I created a function that asks for user inout via a prompt, I did try using input HTML but I didnt want to divet atention away from my js.file. This commit point should reflect the "writing mileage function" commit.
-
-Also once I finished my code where user input function inputs text via "p" through the updateDom code. I console an ever updating array of text that changes based on user input. my array function will rewrite and increae with every attempt by my user. This portion of code should reflect "learning about arrays" portion.
-
-Lastly I had to fix numerous issues as you can find through out my code where I had to comment out code in varying states of completeness."fixing issues".
-
-I have currently added commented sections for my code showing my follow along,
-
-Devtools has made things a little easier to see some execution but im still learning basics and fixing everything via VS code. The I notice that my IDE shows me syntax issues a lot more in the past and unfortunately i dont know  what settings or extensions I changed so I am relying on dev tools to catch execution errors mainly because live server is much easier to access than actually running my code via terminal. 
-
-lastly my code has been refactored into a game which i have taking advantage of office hours and the tutor center to make sure i'm on the right track. I will should adjusted all my "var" to use either let or const currently. And have made sure my code is executable and fun and reduced it to its least redundant parts. All my cide excutes with one on click button, my code keeps tracks of game attempts, there is a good breath of trivia, and the code is easy to read and follow along with. I have also added a reset button to clear the div of too many logged attempts but it does not refresh the game.
+Thus far all my code executes on my event listener via my submit button which lable and give id "replay" and to the user it shows attempts.
---
+++ 008/015/main.js
@@ -0,0 +1,217 @@
+// A Bridge Too Far: The Kings Inquiry HTML/Text Based Game.
+
+// Key phrasing
+
+const kinglyAnswer = '"African or European?"';
+let count = 0;
+let rateCount = 0;
+const rateSuccessArray = [];
+const win = 1;
+const lose = 0;
+const winTxt = "Tally!";
+const loseTxt = "Oh Peril! the King's man hath lost his footing off the bridge of death!";
+ 
+
+//updateDOM
+
+const updateDOM = (kingsInquiry) => {
+    if (kingsInquiry == "african or european") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        console.log(winTxt);
+        return;
+    }
+    else if (kingsInquiry == "i am king i'm supposed to know these things") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        console.log(winTxt);
+        return;
+    }
+    else if (kingsInquiry == "a kingly answer") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        console.log(winTxt);
+        return;
+    }
+    else if (kingsInquiry == "five") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        console.log(winTxt);
+        return;
+    }
+    else if (kingsInquiry == "holy hand grenade of antioch") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        console.log(winTxt);
+        return;
+    }
+    else if (kingsInquiry == "I am authur, king of the britains") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        console.log(winTxt);
+        return;
+    }
+    else if (kingsInquiry == "i am your king") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        console.log(winTxt);
+        return;
+    }
+    else if (kingsInquiry == "you don't vote for kings") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        console.log(winTxt);
+        return;
+    }
+    else if (kingsInquiry == "be quiet!") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        console.log(winTxt);
+        return;
+    }
+    else if (kingsInquiry == "now, stand aside worthy adversary!") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        console.log(winTxt);
+        return;
+    }
+    else if (kingsInquiry == "on second thought, let's not go to camelot. 'tis a silly place") {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The King's answer be: " + kinglyAnswer
+        divEl.appendChild(p);
+        rateCount;
+        rateCount += 1;
+        rateSuccessArray.push(win);
+        console.log(winTxt);
+        return;
+    }    
+    else {
+        let divEl = document.querySelector('#output')
+        let p = document.createElement('p')
+        p.textContent = "The hour is past and thou art DOOMED!!!!!"
+        divEl.appendChild(p);
+        alert("Thou has been cast into the Gorge of Eternal Peril!");
+        rateCount;
+        rateSuccessArray.push(lose);
+        console.log(loseTxt);
+        return;
+    }
+}
+
+let buttonCount = document.getElementById("replay")
+replay.onclick = function () {
+    count;
+    count += 1;
+    replay.innerHTML = "Attempt: " + count;
+    return count;
+};
+
+//update the DOM
+
+const updateDOMArray = (kingsInquiry) => {
+        let divEl = document.querySelector('#array')
+        let a = document.createElement('a')
+        a.textContent = '"' + "The King's answer was: " + "'" + kingsInquiry + "' " + '"; '
+        divEl.appendChild(a);
+        return;
+        }
+
+// Play the game/ attemp again
+
+document.querySelector('#replay').addEventListener('click', () => {
+    buttonCount;
+    let swerian = document.getElementById("inputTxt").value;
+    let kingsInquiry = swerian.toLowerCase();
+    let scribe = console.log("They transcribed " + '"' + kingsInquiry + '"');
+    scribe;
+    updateDOM(kingsInquiry);
+
+    //div "output" split into array. Array increase with attempt.
+
+    updateDOMArray(kingsInquiry);
+    const kingsRecord = (document.getElementById("array")).textContent.split('; ');
+    const noNullKingsArray = kingsRecord.filter(element => element !== '');
+    console.log(noNullKingsArray);
+    noNullKingsArray.push('I told you that you were doomed! Oh but nobody listens to me!');
+    
+    // calculate success rate
+    
+    const rateSuccess = (rateCount / count) * 100;
+    const ratePercent = Math.round(rateSuccess) + "%";
+    
+
+    // Give success rate Array with For Loop.
+    let i = 0
+    let sumSuccesses = 0;
+    let appendDivElRate = "; Success Rates Array: ";
+    let appendSuccessArray = "; The scribe has thou averaged plainy: ";
+    /*
+    for (let i = 0; i < rateSuccessArray.length; i++) {
+        sumSuccesses = sumSuccesses + rateSuccessArray[i];
+    };
+    */
+    while (i <= rateSuccessArray.length) {sumSuccesses = sumSuccesses + rateSuccessArray[i];
+        return sumSuccesses;
+    };
+    let rateCounting = sumSuccesses / count;
+    const success = rate.innerHTML = "Success: " + ratePercent + appendDivElRate + rateSuccessArray + appendSuccessArray + rateCounting;
+    success;
+    //input.style.display = 'none';
+});
+
+//refresh Output div. All code is reset including the array.
+
+document.querySelector('#refresh').addEventListener('click', () => {
+    document.getElementById("output").innerHTML = "";
+});
