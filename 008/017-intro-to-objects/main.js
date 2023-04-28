e5c236a4651925ad1920f3ac132bea57894beb5f
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-04-28 11:14:20 -0700

    intro to objects

--- 008/017-intro-to-objects/main.js
+++ 008/017-intro-to-objects/main.js
@@ -1,6 +1,19 @@
-const myObj = {}
+const myObj = {
+    name: 'Mac Mac',
+    age: 32,
+    hobbies: ['coding', 'gaming', 'reading', 'self-defense'],
+    mobility: {
+        wheelchair: 'manual',
+        brand: 'Quickie'
+    },
+    myGreeting: function (person) {
+        return 'O...M...G...Hola Mundo $(person)'
+    }
+}
+const greeting = myObj.myGreeting('Matthew')
 const myArr = []
+const myNumber = 1
 
-console.log(typeof myObj);
-console.log(typeof myArr);
-console.log(myArr === myObj);
+console.log(typeof myObj)
+console.log(typeof myArr)
+console.log(myArr === myObj)
