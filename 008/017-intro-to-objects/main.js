38763305db0b883987983e76082d0f1613c69588
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-04-28 11:48:23 -0700

    the this

--- 008/017-intro-to-objects/main.js
+++ 008/017-intro-to-objects/main.js
@@ -7,12 +7,16 @@ const myObj = {
         brand: 'Quickie'
     },
     myGreeting: function (person) {
-        console.log(this)
-        return '$(this.name) who loves to code $(this.hobbies) O...M...G...Hola Mundo $(person)'
+        let i = 0
+        console.log(myObj.hobbies.length)
+        while (i < myObj.hobbies.length) {
+            console.log('$(this.name) has $(this.hobbies)')
+        };
+        system.out.println('$(this.name) who loves $(this.hobbies) says "O...M...G...Hola Mundo $(person)"')
     }
 }
 const greeting = myObj.myGreeting('Matthew')
-console.log(greeting)
+greeting;
 const myArr = []
 const myNumber = 1
 
