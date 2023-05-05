ee7caf70e80ef3f802afd169a9f9847df03af26e
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-05-05 11:29:22 -0700

    my decision logic

--- 100-my-app/main.js
+++ 100-my-app/main.js
@@ -1,10 +1,5 @@
-function updateDOM (input, checkBoxed) {
-    let divOutput = document.querySelector('#output')
-    let p = document.createElement('p')
-    p.textContent = "Thanks user here is your greeting: " + "\n\n" + ' "' + input + '"' + ". And my morning routine is:" +checkBoxed;
-    divOutput.appendChild(p);
-    return;
-};
+// global variables
+
 const hoursForActivity = 12;
 const wholeDay = "You still have " + hoursForActivity + "house left in your day";
 let input = document.getElementById("inputTxt");
@@ -12,32 +7,50 @@ const validation = [];
 let checkBoxed = [];
 let clear = input.innerText = '';
 
+// functions
+
 const checked = document.querySelectorAll('[type="checkbox"]').forEach(item => {
     if (item.checked === true) {
+        taskTime = parseInt(item.value);
         checkBoxed.push(item.value);
+        for (let i = 0; i < checkBoxed.length; i++) {
+            const time = checkBoxed[i];
+            hoursForActivity -= time;
+        }
     } else {
-
+        wholeDay;
     }
 });
+
+function updateDOM (input, checked) {
+    let divOutput = document.querySelector('#output')
+    let p = document.createElement('p')
+    checked;
+    p.textContent = "Thanks user here is your greeting: " + "\n\n" + ' "' + input + '"' + ". And my morning routine leaves me:" + checked + " hours.";
+    divOutput.appendChild(p);
+    return;
+};
+
 function validate(input) {
     if (input == '' || input == null || input == undefined) {
         validation.push("please enter something");
-        updateDOM(validation)
+        updateDOM(validation, checked)
     }
     else if (input.includes == [0-9]) {
         validation.push("nice try, but please a word or phrase");
-        updateDOM(validation)
+        updateDOM(validation, checked)
     }
     else {
-        updateDOM(input.value)
+        updateDOM(input.value, checked)
     }
 }
 
+// submitting the form
 document.getElementById('replay').addEventListener('click', (e) => {
     e.preventDefault();
     console.log(input.includes == [0 - 9]);
-    checked;
-    validate(input, checkBoxed);
+    //checked;
+    validate(input);
     console.log(input.value)
     console.log(checkBoxed)
     clear;
