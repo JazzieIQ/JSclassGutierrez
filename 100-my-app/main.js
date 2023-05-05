4d903addd256cb975f1603f5c89dc10fca1a580a
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-05-04 21:48:53 -0700

    my decision logic

--- 100-my-app/main.js
+++ 100-my-app/main.js
@@ -1,32 +1,44 @@
-function updateDOM (input) {
+function updateDOM (input, checkBoxed) {
     let divOutput = document.querySelector('#output')
     let p = document.createElement('p')
-    p.textContent = "Thanks user. Here is your greeting: " + '"' + input.value + '"';
+    p.textContent = "Thanks user here is your greeting: " + "\n\n" + ' "' + input + '"' + ". And my morning routine is:" +checkBoxed;
     divOutput.appendChild(p);
     return;
 };
+const hoursForActivity = 12;
+const wholeDay = "You still have " + hoursForActivity + "house left in your day";
 let input = document.getElementById("inputTxt");
 const validation = [];
-let clear = input.innerText= '';
+let checkBoxed = [];
+let clear = input.innerText = '';
+
+const checked = document.querySelectorAll('[type="checkbox"]').forEach(item => {
+    if (item.checked === true) {
+        checkBoxed.push(item.value);
+    } else {
+
+    }
+});
 function validate(input) {
     if (input == '' || input == null || input == undefined) {
         validation.push("please enter something");
         updateDOM(validation)
     }
-    else if (input.value == input.value.toString([0-9])) {
+    else if (input.includes == [0-9]) {
         validation.push("nice try, but please a word or phrase");
         updateDOM(validation)
     }
     else {
-        updateDOM(input)
+        updateDOM(input.value)
     }
 }
 
 document.getElementById('replay').addEventListener('click', (e) => {
     e.preventDefault();
+    console.log(input.includes == [0 - 9]);
+    checked;
+    validate(input, checkBoxed);
     console.log(input.value)
-    console.log((input.value != (/^[A-Za-z]+$/)))
-    validate(input);
-    console.log()
+    console.log(checkBoxed)
     clear;
     })
