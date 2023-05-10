64899dc90d8022da9493172e86341dad5053dfd8
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-05-10 13:38:27 -0700

    my attempt at oop coding challenge

--- 013/027-intro-to-oop/main.js
+++ 013/027-intro-to-oop/main.js
@@ -4,8 +4,14 @@ class person {
         this.lastname = lastname;
         this.age = age;
     }
+    getFullName() {
+        return `${this.firstname} ${this.lastname}`;
+    }
+    setFullName(newFullName) {
+        this._fullname = newFullName;
+    }
     greeting() {
-        console.log(`Hello, my name is ${this.firstname} and I am ${this.age} years old.`);
+        console.log(`Hello, my name is ${this.FullName} and I am ${this.age} years old.`);
     }
 }
 class student extends person {
@@ -13,13 +19,29 @@ class student extends person {
         super(firstname, lastname, age);
         this.classList = classList;
     }
+    getClassList() {
+        return this.classList;
+    }
+    setClassList(newClassList) {
+        this.classList = newClassList;
+    }
     greeting() {
         console.log(`Hello, my name is ${this.firstname} and I am ${this.age} years old and am taking the following classes: ${this.classList}`);
     }
-
+    
 }
+// Mac Mac
 
 const MacMac = new student("Mac Mac", "Gutierrez", "32", ["CIT-93", "CIT-63", "CIT-28"]);
 MacMac.greeting();
-const Stacia = new person("Stacia", "Gutierrez", "29");
+console.log(MacMac.getFullName());
+MacMac.setClassList(["CIT-93", "CIT-63", "CIT-28", "CIT-95", "MUS-10B", "POLCI-5", "POLCI-13"]);
+console.log(MacMac.getClassList());
+
+//Other Students
+
+const Jazmine = new person("Jazmine", "Gutierrez", "30");
+const Stacia = new student("Stacia", "Gutierrez", "29", ["Eng1A", "Eng1B", "Eng1C"]);
 Stacia.greeting();
+Stacia.setClassList(["Eng1A", "Eng1B", "Eng1C", "MUS-10B"]);
+console.log(Stacia.getFullName() + ':\n' + Stacia.getClassList());
